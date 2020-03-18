5f0d29fcfaee2a95d5219ea1e73ccb070dfec756
arnaudambro <arnaud.ambroselli@yahoo.fr>
2020-03-18 15:45:54 +0100

    Updates

---
+++ precache-manifest.1689c50654dea06b673402a993c918dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c593399e516b2994edf9b5150602599d",
+    "url": "/daily-rates-convertor/index.html"
+  },
+  {
+    "revision": "ecd182b52f258114e7c9",
+    "url": "/daily-rates-convertor/static/js/2.fec3b8cd.chunk.js"
+  },
+  {
+    "revision": "8231308fcbfa148cd407b1df32ebf0c7",
+    "url": "/daily-rates-convertor/static/js/2.fec3b8cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f394b8ee3f2c35fb9d",
+    "url": "/daily-rates-convertor/static/js/main.5e35fd74.chunk.js"
+  },
+  {
+    "revision": "3535a42dc54393653b1d",
+    "url": "/daily-rates-convertor/static/js/runtime-main.c69480ba.js"
+  }
+]);

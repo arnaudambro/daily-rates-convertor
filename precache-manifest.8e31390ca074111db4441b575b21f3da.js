3d856a1312d33009520a8041be47b6e2b6a2372d
arnaudambro <arnaud.ambroselli@yahoo.fr>
2020-03-18 16:21:10 +0100

    Updates

---
+++ precache-manifest.8e31390ca074111db4441b575b21f3da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacc26bf9479f1396e58ee4eeb2ef623",
+    "url": "/daily-rates-convertor/index.html"
+  },
+  {
+    "revision": "522ffc421a9fb679695e",
+    "url": "/daily-rates-convertor/static/js/2.9ca517b8.chunk.js"
+  },
+  {
+    "revision": "8231308fcbfa148cd407b1df32ebf0c7",
+    "url": "/daily-rates-convertor/static/js/2.9ca517b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b316fc4146ffb8c38bb",
+    "url": "/daily-rates-convertor/static/js/main.31b24915.chunk.js"
+  },
+  {
+    "revision": "3535a42dc54393653b1d",
+    "url": "/daily-rates-convertor/static/js/runtime-main.c69480ba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/daily-rates-convertor/precache-manifest.fb57c1d83d1be1dbcde136fbd6c08285.js"
+  "/daily-rates-convertor/precache-manifest.8e31390ca074111db4441b575b21f3da.js"
 );
 
 self.addEventListener('message', (event) => {
